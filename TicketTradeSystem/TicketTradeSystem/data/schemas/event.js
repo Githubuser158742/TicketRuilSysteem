654830c6ac16df84e6364e664161aa7d69a3ccef
hannesdeclercq <hannes.declercq@gmail.com>
2015-12-17 14:56:26 +0100

    WWW

---
+++ TicketTradeSystem/TicketTradeSystem/data/schemas/event.js
@@ -0,0 +1,11 @@
+﻿var mongoose = require('mongoose');
+
+var eventSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String },
+    price: { type: Number },
+    userid: { type: String, required: true },
+    createdOn: { type: Date, default: Date.now }
+});
+
+module.exports = ticketSchema;
