55f737738a1dc779206c654798c3efe037ae8122
hannesdeclercq <hannes.declercq@gmail.com>
2015-12-27 19:10:40 +0100

    users

--- TicketTradeSystem/TicketTradeSystem/data/models/usersRepo.js
+++ TicketTradeSystem/TicketTradeSystem/data/models/usersRepo.js
@@ -5,23 +5,22 @@ var usersRepo = (function () {
     var User = require('./user.js');
     var changeUser = function (user, changes, next) {
         var changedUser = user;
+        if (!user.fb) { changedUser.local.email = changes.email }
         changedUser.email = changes.email;
         changedUser.city = changes.city;
         changedUser.firstname = changes.firstname;
         changedUser.lastname = changes.lastname;
-        changedUser.save(function (err) {
-            if (err) return next(err)
-            // What's happening in passport's session? Check a specific field...
-            console.log("Before relogin: " + req.session.passport.user.changedField)
-            
-            req.login(user, function (err) {
+        changedUser.save(function (err, user) {
+            if (err) return next(err)            
+            user.login(user, function (err) {
                 if (err) return next(err)
-                
-                console.log("After relogin: " + req.session.passport.user.changedField)
-                res.send(200)
             })
-        })
-    }
+        });
+    };
+    return {
+        model: User,
+        changeUser: changeUser
+    };
 }());
 
 module.exports = usersRepo;
