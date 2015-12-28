4e658806f0a79dd6aeace6cee905324da91c9397
Jim Butseraen <jim.butseraen@gmail.com>
2015-12-28 12:36:58 +0100

    typo fixed

--- TicketTradeSystem/data/schemas/trade.js
+++ TicketTradeSystem/data/schemas/trade.js
@@ -3,7 +3,7 @@ var tradeSchema = new mongoose.Schema({
     _ticket: {type: Schema.ObjectId, ref: 'Ticket'},
     _user: {type: Schema.ObjectId, ref: 'User'},
     amount: {type: Number},
-    createdOn: {type: Data, default: Date.now}
+    createdOn: {type: Date, default: Date.now}
 });
 
 module.exports = tradeSchema;
