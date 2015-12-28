04b33c67da4e317b5427011f780ad04e591ba9b8
Jim Butseraen <jim.butseraen@gmail.com>
2015-12-28 16:25:31 +0100

    Tests: the prequel

---
+++ TicketTradeSystem/tests/mocha/TicketTest.js
@@ -0,0 +1,34 @@
+﻿var expect = require('chai').expect;
+var mongoose = require('mongoose');
+var mockgoose = require('mockgoose');
+//mock database
+mockgoose(mongoose);
+
+var Ticket = mongoose.model('Ticket', {
+    _event: { type: Schema.ObjectId, ref: 'Event' },
+    _user: { type: Schema.ObjectId, ref: 'User' },
+    price: { type: Number },
+    amount: { type: Number },
+    createdOn: { type: Date, default: Date.now }
+});
+
+beforeEach(function (done) {
+    mockgoose.reset();
+    Ticket.create({
+        _event: 1,
+        _user: 1,
+        price: 5,
+        amount: 10,
+    }, function (err, model) {
+        done(err);
+    });
+});
+
+afterEach(function (done) {
+    mockgoose.reset();
+    done();
+});
+
+
+
+
