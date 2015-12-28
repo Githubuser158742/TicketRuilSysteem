8ad4b6ea3161cafdc4edb9cce0a2eb34cc768a36
hannesdeclercq <hannes.declercq@gmail.com>
2015-12-28 20:58:07 +0100

    Trade

--- TicketTradeSystem/data/models/tradesRepo.js
+++ TicketTradeSystem/data/models/tradesRepo.js
@@ -32,6 +32,36 @@ var Trade = require('./trade.js');
                 next(null, docs);
             });
         },
+        getTradesByIdSeller = function (id, next) {
+            Trade.find({})
+        .populate('_user')
+        .populate({
+                path: '_ticket',
+                populate : {
+                    path: '_event',
+                    model: 'Event'
+                }
+            })
+        .exec(function (err, docs) {
+                if (err) {
+                    console.log(err);
+                    next(err, null);
+                }
+                var options = { path: '_ticket._user', model: 'User' };
+                Trade.populate(docs, options, function (err, trades) {
+                    var trades2 = [];
+                    trades.forEach(function (trade) {
+                        if (trade._ticket._user._id == id) { 
+                            trades2.push(trade);
+                        }
+                    });
+                    console.log(trades);
+                    console.log("-------------");
+                    console.log(trades2);
+                    next(null, trades2);
+                });
+            });
+        },
         createTrade = function (trade, next) {
             var trade = new Trade({
                 amount: trade.body.amount,
@@ -50,6 +80,7 @@ var Trade = require('./trade.js');
         model: Trade,
         getAllTrades: getAllTrades,
         getTradesByIdUser: getTradesByIdUser,
+        getTradesByIdSeller: getTradesByIdSeller,
         createTrade: createTrade
     };
 }());
--- TicketTradeSystem/routes/trades.js
+++ TicketTradeSystem/routes/trades.js
@@ -8,12 +8,11 @@ var isAuthenticated = require('./middleware/isAuthenticated.js');
 var tradesRepo = require('../data/models/tradesRepo');
 
 router.get('/', isAuthenticated,function (req, res) {
-    tradesRepo.getAllTrades(function (err, trades) {
+    tradesRepo.getTradesByIdSeller(req._passport.session.user, function (err, trades) {
         if (err) {
             res.status(500).send('server error - trades');
             res.end();
         }
-        console.log(trades);
         res.render('trades/index', { title: 'Trades', tradeslist: trades });
     });
 });
