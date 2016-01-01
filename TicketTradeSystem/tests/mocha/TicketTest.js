35b5b0576cc54306906eb741f49bd6d1ba8393d9
Jim Butseraen <jim.butseraen@gmail.com>
2016-01-01 22:20:45 +0100

    test test

--- TicketTradeSystem/tests/mocha/TicketTest.js
+++ TicketTradeSystem/tests/mocha/TicketTest.js
@@ -1,8 +1,20 @@
-﻿var expect = require('chai').expect;
-var mongoose = require('mongoose');
+﻿//var assert = require('assert');
+var expect = require('chai').expect;
+var Mongoose = require('mongoose').Mongoose;
+var mongoose = new Mongoose();
 var mockgoose = require('mockgoose');
-//mock database
-mockgoose(mongoose);
+
+//describe('Test Suite 1', function() {
+//    it('Test 1', function() {
+//        assert.ok(true, "This shouldn't fail");
+//    })
+
+//    it('Test 2', function() {
+//        assert.ok(1 === 1, "This shouldn't fail");
+//        assert.ok(false, "This should fail");
+//    })
+//})
+
 
 var Ticket = mongoose.model('Ticket', {
     _event: { type: Schema.ObjectId, ref: 'Event' },
@@ -12,22 +24,66 @@ var Ticket = mongoose.model('Ticket', {
     createdOn: { type: Date, default: Date.now }
 });
 
-beforeEach(function (done) {
-    mockgoose.reset();
-    Ticket.create({
-        _event: 1,
-        _user: 1,
-        price: 5,
-        amount: 10,
-    }, function (err, model) {
-        done(err);
-    });
-});
+describe('Test add ticket', function (){
+    it('should create ticket', function (done) {
+        Ticket.create({
+            _event: 1,
+            _user: 1,
+            price: 5,
+            amount: 10,
+        }, function (err) {
+            done(err)
+        })
+    })
+})
 
-afterEach(function (done) {
-    mockgoose.reset();
-    done();
-});
+
+
+//beforeEach(function (done) {
+//    mockgoose.reset();
+//    Ticket.create({
+//        _event: 1,
+//        _user: 1,
+//        price: 5,
+//        amount: 10,
+//    }, function (err, model) {
+//        done(err);
+//    });
+//});
+
+//afterEach(function (done) {
+//    mockgoose.reset();
+//    done();
+//});
+
+//describe('Tickets', function () {
+//    before(function (done) {
+//        mongoose.connect('mongodb://hannes:abc123@ds058048.mongolab.com:58048/tts', function (err) {
+//            done(err)
+//        })
+//    })
+    
+//    it('should create ticket', function (done) {
+//        Ticket.create({
+//            _event: 1,
+//            _user: 1,
+//            price: 5,
+//            amount: 10,
+//        }, function (err) {
+//            done(err)
+//        })
+//    })
+    
+//    it('should find ticket', function (done) {
+//        Event.findOne({ _event: 1 }, function (err, ticket) {
+//            expect(ticket._user).to.equal(1)
+//            expect(ticket.price).to.equal(5)
+//            expect(ticket.amount).to.equal(10)
+//            done(err);
+//        })
+//    })
+//})
+
 
 
 
